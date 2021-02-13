3e142fdb311a5e663119293be85cd90ac7fafe6e
Hadrien Lanneau <hadrien.lanneau@gogowego.com>
2021-02-13 17:44:06 +0100

    Admin index

---
+++ pages/admin/Admin.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import Box from 'components/Box';
+
+export const Admin: FC = () => {
+  return <Box>Admin</Box>;
+};
+
+export default Admin;
--- pages/api/version.js
+++ pages/api/version.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import { version } from 'package.json';
+
 export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
-}
+  res.status(200).send(version);
+};
