231c9e6124ecd9b98c39a700cf1201d76081dee8
Hadrien Lanneau <hadrien.lanneau@gogowego.com>
2021-10-14 13:41:57 +0200

    Create new basket if previous one is not pending or deprecated (as distribution dates)

--- sql/6- functions.sql
+++ sql/6- functions.sql
@@ -14,7 +14,10 @@ begin
     from distribution
     into distribution_id, distribution_price
     left join product_in_distribution on product_in_distribution.fk_distribution = distribution.id
-    where product_in_distribution.id = product_id
+    where
+      product_in_distribution.id = product_id and
+      distribution.start_at < CURRENT_DATE and
+      distribution.close_at > CURRENT_DATE
     fetch first row only;
 
   if (distribution_id is null)
@@ -22,16 +25,22 @@ begin
     return false;
   end if;
 
-  select basket.id
+  select basket.id, distribution.start_at
     from basket
     into basket_id
-    where basket.fk_customer = auth.uid() and basket.status = 0
-    fetch first row only;
+    left join product_in_basket on product_in_basket.fk_basket = basket.id
+    left join product_in_distribution on product_in_basket.fk_product = product_in_distribution.id
+    left join distribution on product_in_distribution.fk_distribution = distribution.id
+    where
+          basket.fk_customer = auth.uid()
+      and distribution.start_at < CURRENT_DATE
+      and distribution.close_at > CURRENT_DATE;
   
   if (basket_id is null)
   then
-    return false;
-    -- create a new basket
+    insert into basket (fk_customer, status)
+      values (auth.uid(), 0)
+      returning basket.id into basket_id;
   end if;
   
   insert into product_in_basket (fk_basket, fk_product, unit_price, quantity)
